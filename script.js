aae6251d33a249217b6430feefffd0bca3415491
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-01 19:51:47 +0300

    javascript tamamlandı

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ const play = document.getElementById("play");
 const control = document.getElementById("control");
 const age = document.getElementById("age");
 
-let yas = prompt("Lütfen yaşınızı giriniz?: ");
+let yas = prompt("Lütfen yaşınızı giriniz? ");
 let resit = "REŞİTSİNİZ";
 let resitDegil = "REŞİT DEĞİLSİNİZ";
 
 count = "0";
+zero = " ";
+
 reset.onclick = function () {
   age.textContent = count;
+  control.textContent = zero;
 };
-/* play.onclick= function{
-
-} */
+play.onclick = function () {
+  location.reload();
+};
 if (yas >= 18) {
+  age.textContent = yas;
   control.textContent = resit;
 } else {
+  age.textContent = yas;
   control.textContent = resitDegil;
 }
